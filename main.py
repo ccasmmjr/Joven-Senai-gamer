a8e4474012d06000b490eb2ddeb7d0154d32872c
ccasmmjr <84786554+ccasmmjr@users.noreply.github.com>
2024-06-14 12:40:32 -0300

    main com interface e login e cadastro feito

--- main.py
+++ main.py
@@ -1,7 +1,80 @@
 import xmlrpc.client
 import sys
+import tkinter as tk
+from tkinter import messagebox
 
-nome = "oi"
-senha = "2"
 serverCL = xmlrpc.client.ServerProxy("http://localhost:8000")
-serverCL.add_user(senha, nome, senha)
+
+def fazer_login():
+    username = username_entry.get()
+    senha = senha_entry.get()
+    adm = admin_var.get()
+    if admin_var.get() == 1:
+        tipo_usuario = "Administrador"
+        if serverCL.check_user_credentials(senha, username, senha, adm):
+            print("A senha está correta para o usuário.")
+            abrir_outra_janela()
+            janela.withdraw()
+        else:
+            print("A senha está incorreta ou o usuário não existe.")
+    else:
+        if serverCL.check_user_credentials(senha, username, senha, adm):
+            print("A senha está correta para o usuário.")
+            abrir_outra_janela()
+            janela.withdraw()
+        else:
+            print("A senha está incorreta ou o usuário não existe.")
+        tipo_usuario = "Usuário normal"
+    messagebox.showinfo("Login", f"Username: {username}\nSenha: {senha}\nTipo de usuário: {tipo_usuario}")
+
+def fazer_cadastro():
+    username = username_entry.get()
+    senha = senha_entry.get()
+    adm=admin_var.get()
+    if admin_var.get() == 1:
+        tipo_usuario = "Administrador"
+        serverCL.add_user(senha, username, senha, adm)
+    else:
+        serverCL.add_user(senha, username, senha, adm)
+        tipo_usuario = "Usuário normal"
+    messagebox.showinfo("Cadastro", f"Cadastro realizado!\nUsername: {username}\nSenha: {senha}\nTipo de usuário: {tipo_usuario}")
+
+def abrir_outra_janela():
+    outra_janela = tk.Toplevel(janela)
+    outra_janela.title("Outra Janela")
+    label = tk.Label(outra_janela, text="Você fez login com sucesso!")
+    label.pack()
+    #janela.destroy()
+
+
+# Cria a janela principal
+janela = tk.Tk()
+janela.title("Login e Cadastro")
+
+# Cria os widgets
+username_label = tk.Label(janela, text="Username:")
+username_entry = tk.Entry(janela)
+
+senha_label = tk.Label(janela, text="Senha:")
+senha_entry = tk.Entry(janela, show="*")
+
+admin_var = tk.IntVar()
+admin_checkbox = tk.Checkbutton(janela, text="Administrador", variable=admin_var)
+
+login_button = tk.Button(janela, text="Login", command=fazer_login)
+cadastro_button = tk.Button(janela, text="Cadastro", command=fazer_cadastro)
+
+# Coloca os widgets na janela
+username_label.grid(row=0, column=0, sticky="e")
+username_entry.grid(row=0, column=1)
+senha_label.grid(row=1, column=0, sticky="e")
+senha_entry.grid(row=1, column=1)
+admin_checkbox.grid(row=2, columnspan=2)
+login_button.grid(row=3, column=0, pady=10)
+cadastro_button.grid(row=3, column=1, pady=10)
+
+# Inicia o loop da aplicação
+janela.mainloop()
+
+
+

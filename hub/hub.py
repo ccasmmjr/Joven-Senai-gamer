5b5bba96a6b92626701ad722a392a8a53f99a86e
ccasmmjr <84786554+ccasmmjr@users.noreply.github.com>
2024-06-14 13:11:02 -0300

    hub principais fucoes criadas

--- hub/hub.py
+++ hub/hub.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+import xmlrpc.server
 # Inicialize o aplicativo do Firebase com suas credenciais
 cred = credentials.Certificate("/app/jovemsenai-2fbc6-firebase-adminsdk-wlzwy-50db1941db.json")
 firebase_admin.initialize_app(cred, {
@@ -9,6 +10,24 @@ firebase_admin.initialize_app(cred, {
 })
 ref = db.reference('/')
 
-ref.set({
-    'message': 'Hello, Firebase!'
-})
+# Escreva dados no banco de dados
+class MyserverHUB:
+    def add_game(self, nome, detalhes):
+        
+            ref.child('game').child(nome).set({
+                'detalhes': detalhes
+            })
+            
+    def view_game(self, nome):
+        
+             user_ref = ref.child('game').child(nome)
+             user_data = user_ref.get()
+             if user_data is not None:
+                return user_data.get('senha')
+             else:
+                return None
+      
+    
+server = xmlrpc.server.SimpleXMLRPCServer(("0.0.0.0",8080), allow_none=True)
+server.register_instance(MyserverHUB)
+server.serve_forever()

518df8497b2afb2f9a9be43f2dfa747baa53d411
ccasmmjr <84786554+ccasmmjr@users.noreply.github.com>
2024-06-14 13:58:04 -0300

    hubuser base feito

--- hubuser.py
+++ hubuser.py
@@ -1,10 +1,26 @@
 import xmlrpc.client
 import sys
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import scrolledtext
+
 serverHUB = xmlrpc.client.ServerProxy("http://localhost:8080")
 def abrir_outra_janela(janela):
     outra_janela = tk.Toplevel(janela)
     outra_janela.title("Outra Janela")
     label = tk.Label(outra_janela, text="Você fez login com sucesso!")
     label.pack()
+    data = serverHUB.view_game("a")
+    scroll_text = scrolledtext.ScrolledText(outra_janela, wrap=tk.WORD, width=50, height=20)
+    scroll_text.pack(padx=10, pady=10)
+    formatted_data = format_data(data)
+    scroll_text.insert(tk.END, formatted_data)
+
+
+def format_data(data):
+    formatted_data = ""
+    for key, value in data.items():
+        formatted_data += f"{key}: {value}\n"
+    return formatted_data
+
+
+
